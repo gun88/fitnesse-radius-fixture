4551a0f4dc2c3fa9c72468becb51b8dfa31e2e2f
tpomante <tommaso.pomante@hpecds.com>
2021-10-23 19:05:32 +0200

    commit pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   readmeUrl: 'https://raw.githubusercontent.com/gun88/fitnesse-radius-fixture/master/README.md',
-  downloadUrl: 'https://github.com/gun88/fitnesse-radius-fixture/releases/download/v1.0.0/fitnesse-radius-fixture-1.0.0.jar',
+  downloadUrl: 'https://github.com/gun88/fitnesse-radius-fixture/releases/download/v1.0.1/fitnesse-radius-fixture-1.0.1.jar',
   gitHubUrl: 'https://github.com/gun88/fitnesse-radius-fixture',
   wikiUrl: 'https://github.com/gun88/fitnesse-radius-fixture/wiki',
   projectsUrl: 'https://raw.githubusercontent.com/gun88/fitnesse-radius-fixture/gh-pages/projects.json'
